3d6a5b9c6a0cf7f2d400e8d739bf7ff199d3565c
unknown <Ln@leavingstone.com>
2022-05-02 12:57:14 +0400

    throttler add

--- src/Layout/Scroll/Scroll.js
+++ src/Layout/Scroll/Scroll.js
@@ -1,60 +1,10 @@
 import React, { useEffect, useState } from 'react';
 
-// const debounce = (func, wait) => {
-//   let timeout;
-//   return (...args) => {
-//     const later = () => {
-//       timeout = null;
-
-//       func(...args);
-//     };
-//     clearTimeout(timeout);
-
-//     timeout = setTimeout(later, wait);
-//   };
-// };
-
-// let throttlePause;
-
-// const throttle = (callback, time) => {
-//   //don't run the function if throttlePause is true
-//   if (throttlePause) return;
-//   console.log(throttlePause);
-
-//   //set throttlePause to true after the if condition. This allows the function to be run once
-//   throttlePause = true;
-
-//   //setTimeout runs the callback within the specified time
-//   setTimeout(() => {
-//     callback();
-
-//     //throttlePause is set to false once the function has been called, allowing the throttle function to loop
-//     throttlePause = false;
-//   }, time);
-// };
-
 function SectionScrolling(props) {
   const { children, currView, setCurrView, shouldScrollDisplay } = props;
   const [loading, setLoading] = useState(false);
 
   const [direction, setDirection] = useState('');
-
-  function scrollDown(page) {
-    if (currView === pages.length - 1) return;
-    // console.log('scrolled');
-    setCurrView(currView + 1);
-    const axis = `translateY(-${currView + 1}00vh)`;
-    document.getElementById('wrapper').style.transform = axis;
-  }
-
-  function scrollUp(page) {
-    if (currView === 0) return;
-    // console.log('scrolled');
-
-    setCurrView(currView - 1);
-    const axis = `translateY(-${currView - 1}00vh)`;
-    document.getElementById('wrapper').style.transform = axis;
-  }
 
   const pages = [
     { name: 'welcome', axis: 'translateY(0vh)' },
@@ -64,59 +14,12 @@ function SectionScrolling(props) {
     { name: 'contact', axis: 'translateY(-400vh)' },
   ];
 
-  const MouseWheelHandler = (e) => {
-    if (!shouldScrollDisplay) return;
-    if (loading) return;
-    var event = window.event || e;
-    var delta = Math.max(-1, Math.min(1, event.wheelDelta || -event.detail));
-    if (delta > 0) {
-      if (currView === 0) return;
-      setDirection('up');
-      setLoading(true);
-    }
-    if (delta < 0) {
-      if (currView === pages - 1) return;
-      setDirection('down');
-      setLoading(true);
-    }
-  };
-
-  function throttler(callback, delay = 3000) {
-    let shouldWait = false;
-
-    return (...args) => {
-      if (shouldWait === true) return;
-
-      callback(...args);
-      shouldWait = true;
-
-      setTimeout(() => {
-        shouldWait = false;
-      }, delay);
-    };
-  }
-  const handleThrottle = throttler((e) => MouseWheelHandler(e));
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  // const debounceHandler = useMemo(throttle(MouseWheelHandler(), 500), []);
-
-  const handleKeyDown = (e) => {
-    if (e.key === 'ArrowUp') {
-      if (currView === 0) return;
-      setDirection('up');
-      setLoading(true);
-    }
-    if (e.key === 'ArrowDown') {
-      if (currView === pages - 1) return;
-      setDirection('down');
-      setLoading(true);
-    }
-  };
   console.log(shouldScrollDisplay);
-  if (loading === false) {
-    document.body.addEventListener('mousewheel', handleThrottle, false);
-    document.body.addEventListener('keydown', handleKeyDown, false);
-    document.body.addEventListener('DOMMouseScroll', handleThrottle, false);
-  }
+  // if (loading === false) {
+  //   document.body.addEventListener('mousewheel', handleThrottle, false);
+  //   document.body.addEventListener('keydown', handleKeyDown, false);
+  //   document.body.addEventListener('DOMMouseScroll', handleThrottle, false);
+  // }
 
   useEffect(() => {
     setTimeout(() => {
@@ -126,6 +29,84 @@ function SectionScrolling(props) {
   }, [loading]);
 
   useEffect(() => {
+    const MouseWheelHandler = (e) => {
+      if (!shouldScrollDisplay) return;
+      if (loading) return;
+      var event = window.event || e;
+      var delta = Math.max(-1, Math.min(1, event.wheelDelta || -event.detail));
+      if (delta > 0) {
+        if (currView === 0) return;
+        setDirection('up');
+        setLoading(true);
+      }
+      if (delta < 0) {
+        if (currView === pages - 1) return;
+        setDirection('down');
+        setLoading(true);
+      }
+    };
+
+    function throttler(callback, delay = 1000) {
+      let shouldWait = false;
+
+      return (...args) => {
+        if (shouldWait === true) return;
+
+        callback(...args);
+        shouldWait = true;
+
+        setTimeout(() => {
+          shouldWait = false;
+        }, delay);
+      };
+    }
+    const handleThrottle = throttler((e) => MouseWheelHandler(e));
+
+    const handleKeyDown = (e) => {
+      if (e.key === 'ArrowUp') {
+        if (currView === 0) return;
+        setDirection('up');
+        setLoading(true);
+      }
+      if (e.key === 'ArrowDown') {
+        if (currView === pages - 1) return;
+        setDirection('down');
+        setLoading(true);
+      }
+    };
+
+    document.body.addEventListener('mousewheel', handleThrottle, false);
+    document.body.addEventListener('keydown', handleKeyDown, false);
+    document.body.addEventListener('DOMMouseScroll', handleThrottle, false);
+
+    return () => {
+      document.body.removeEventListener('mousewheel', handleThrottle, false);
+      document.body.removeEventListener('keydown', handleKeyDown, false);
+      document.body.removeEventListener(
+        'DOMMouseScroll',
+        handleThrottle,
+        false
+      );
+    };
+  }, []);
+
+  useEffect(() => {
+    function scrollDown() {
+      if (currView === pages.length - 1) return;
+      setCurrView(currView + 1);
+      const axis = `translateY(-${currView + 1}00vh)`;
+      document.getElementById('wrapper').style.transform = axis;
+    }
+
+    function scrollUp(page) {
+      if (currView === 0) return;
+      // console.log('scrolled');
+
+      setCurrView(currView - 1);
+      const axis = `translateY(-${currView - 1}00vh)`;
+      document.getElementById('wrapper').style.transform = axis;
+    }
+
     if (loading) return;
     if (direction === 'down') scrollDown();
     if (direction === 'up') scrollUp();
