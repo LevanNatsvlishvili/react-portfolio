d43afc263b8587a99e66b54def32c7b047f6bedc
unknown <Ln@leavingstone.com>
2022-05-12 01:03:17 +0400

    social links refactor

--- src/Layout/Header/HeaderContactLinks.js
+++ src/Layout/Header/HeaderContactLinks.js
@@ -3,6 +3,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { paths } from 'Routing/Paths';
 
+const SocialLink = ({ url, img, media }) => (
+  <a className="px-3 " target="_blank" href={url} rel="noreferrer">
+    <div className="text-base flex items-center w-8 h-8 overflow-hidden transition-all duration-150 ease-in hover:w-28">
+      <img width="30px" height="30px" src={img} alt="" className="w-7 h-7" />
+      <p className="ml-2">{media}</p>
+    </div>
+  </a>
+);
+
 function HeaderContactLinks(props) {
   const { navMenu, setNavMenu } = props;
 
@@ -13,43 +22,26 @@ function HeaderContactLinks(props) {
   return (
     <div className="flex items-center w-full fixed w-full px-10 pt-10 z-50">
       <div>
-        <h5>
-          <Link to={paths.home}>Levan Natsvlishvili</Link>
-        </h5>
+        <div className="flex items-center mr-16">
+          <SocialLink
+            url="https://www.linkedin.com/in/levnac"
+            media="Linkedin"
+            img="/images/SocMedia/linkedin.svg"
+          />
+          <SocialLink
+            url="https://github.com/LevanNatsvlishvili"
+            media="Github"
+            img="/images/SocMedia/github2.svg"
+          />
+          <SocialLink
+            url="https://www.upwork.com/freelancers/~01d797802b8c4359ad"
+            media="Linkedin"
+            img="/images/SocMedia/upwork.svg"
+          />
+        </div>
       </div>
       <div className="ml-auto">
         <div className="flex items-center">
-          <div className="flex items-center mr-16">
-            <a
-              className="px-3"
-              target="_blank"
-              href="https://www.linkedin.com/in/levnac/"
-              rel="noreferrer"
-            >
-              <img width="30px" src="/images/SocMedia/linkedin.svg" alt="" />
-            </a>
-            <a
-              className="px-3"
-              target="_blank"
-              href="https://www.upwork.com/freelancers/~01d797802b8c4359ad"
-              rel="noreferrer"
-            >
-              <img width="30px" src="/images/SocMedia/upwork.svg" alt="" />
-            </a>
-            <a
-              className="px-3"
-              target="_blank"
-              href="https://github.com/LevanNatsvlishvili"
-              rel="noreferrer"
-            >
-              <img
-                width="30px"
-                height="22px"
-                src="/images/SocMedia/github2.svg"
-                alt=""
-              />
-            </a>
-          </div>
           <div
             onClick={handleNav}
             className={clsx('navbutton', { 'navbutton-active': navMenu })}
