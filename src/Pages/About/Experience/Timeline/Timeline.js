4d40a2d676e007697c57ef1a327001ca574e5fb2
unknown <Ln@leavingstone.com>
2022-05-11 23:48:33 +0400

    removed unused vars

--- src/Pages/About/Experience/Timeline/Timeline.js
+++ src/Pages/About/Experience/Timeline/Timeline.js
@@ -1,13 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import TimelineDesktop from './TimelineDesktop';
 import TimelineMobile from './TimelineMobile';
-
-const staticPositions = {
-  1: [45],
-  2: [0, 85],
-  3: [0, 40, 85],
-  4: [0, 30, 60, 85],
-};
 
 const Timeline = (props) => {
   const { companies, handleCompany, curr } = props;
