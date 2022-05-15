b90f282c01562094adb7dbebce95316033abb7a3
unknown <Ln@leavingstone.com>
2022-05-15 23:37:38 +0400

    added mobile scrolling

--- src/Layout/Scroll/Scroll.js
+++ src/Layout/Scroll/Scroll.js
@@ -56,12 +56,67 @@ function SectionScrolling(props) {
       setLoading(true);
     }
   };
+
   useEffect(() => {
+    var xDown = null;
+    var yDown = null;
+
+    function getTouches(evt) {
+      return (
+        evt.touches || // browser API
+        evt.originalEvent.touches
+      ); // jQuery
+    }
+
+    function handleTouchStart(evt) {
+      const firstTouch = getTouches(evt)[0];
+      xDown = firstTouch.clientX;
+      yDown = firstTouch.clientY;
+    }
+
+    function handleTouchMove(evt) {
+      if (!xDown || !yDown) {
+        return;
+      }
+
+      var xUp = evt.touches[0].clientX;
+      var yUp = evt.touches[0].clientY;
+
+      var xDiff = xDown - xUp;
+      var yDiff = yDown - yUp;
+
+      if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        /*most significant*/
+        if (xDiff > 0) {
+          /* left swipe */
+        } else {
+          /* right swipe */
+        }
+      } else {
+        if (yDiff > 0) {
+          /* up swipe */
+          setDirection('up');
+          setLoading(true);
+        } else {
+          /* down swipe */
+          setDirection('down');
+          setLoading(true);
+        }
+      }
+      /* reset values */
+      xDown = null;
+      yDown = null;
+    }
+
+    document.body.addEventListener('touchstart', handleTouchStart, false);
+    document.body.addEventListener('touchmove', handleTouchMove, false);
     document.body.addEventListener('mousewheel', handleThrottle, false);
     document.body.addEventListener('keydown', handleKeyDown, false);
     document.body.addEventListener('DOMMouseScroll', handleThrottle, false);
 
     return () => {
+      document.body.removeEventListener('touchstart', handleTouchStart, false);
+      document.body.removeEventListener('touchmove', handleTouchMove, false);
       document.body.removeEventListener('mousewheel', handleThrottle, false);
       document.body.removeEventListener('keydown', handleKeyDown, false);
       document.body.removeEventListener(
