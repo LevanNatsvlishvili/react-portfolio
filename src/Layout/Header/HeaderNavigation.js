340ba5030f4df45eb06f9e8b556c4c93e89c140c
unknown <Ln@leavingstone.com>
2022-05-22 23:50:35 +0400

    nav menu clouds

--- src/Layout/Header/HeaderNavigation.js
+++ src/Layout/Header/HeaderNavigation.js
@@ -9,20 +9,23 @@ function HeaderNavigation(props) {
   return (
     <nav
       className={clsx(
-        'navmenu overflow-hidden ease-linear flex justify-center items-center text-6xl sm:text-8xl text-center absolute z-20',
+        'navmenu  overflow-hidden ease-linear flex justify-center items-center text-6xl sm:text-8xl text-center absolute z-20',
         {
           '-translate-y-[100vh]': !navMenu,
+          'nav-lightning': navMenu,
         }
       )}
     >
-      <div className="cloud-circle cloud-1"></div>
-      <div className="cloud-circle cloud-2"></div>
-      <div className="cloud-circle cloud-3"></div>
-      <div className="cloud-circle cloud-4"></div>
-      <div className="cloud-circle cloud-5"></div>
-      <div className="cloud-circle cloud-6"></div>
-      <div className="cloud-circle cloud-7"></div>
-      <div className="cloud-circle cloud-8"></div>
+      <div className={`cloud-circle cloud-1`}></div>
+      <div className={`cloud-circle cloud-2`}></div>
+      <div className={`cloud-circle cloud-3`}></div>
+      <div className={`cloud-circle cloud-4`}></div>
+      <div className={`cloud-circle cloud-5`}></div>
+      <div className={`cloud-circle cloud-6`}></div>
+      <div className={`cloud-circle cloud-7`}></div>
+      <div className={`cloud-circle cloud-8`}></div>
+      <div className={`cloud-circle cloud-9`}></div>
+      <div className={`cloud-circle cloud-10`}></div>
       <svg width="0" height="0">
         <filter id="filter">
           <feTurbulence
